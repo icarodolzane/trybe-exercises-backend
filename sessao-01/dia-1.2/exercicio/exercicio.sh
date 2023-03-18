4f88271a4e24406be3756648b28f64d9c484cc03
icarodolzane <icarodolzane@live.com>
2023-03-18 14:08:23 -0400

    Finalizado exercicio do dia 1.2

---
+++ sessao-01/dia-1.2/exercicio/exercicio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "docker image build ./ -t cowsay"
+
+echo "docker container run cowsay"
+
+echo "docker container run cowsay Go Trybe!"
+
+echo "docker container run cowsay -f moofasa "#VQV TRYBE""
+
+echo docker container run cowsay -l
